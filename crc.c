0b66928d08c571a6d0e23aac229ee37ea0050afe
Vivek V Hathwar <34102731+vivekhathwar@users.noreply.github.com>
2018-11-26 22:55:59 +0530

    Update crc.c

--- crc.c
+++ crc.c
@@ -1,13 +1,13 @@
 /*Using CRC method for error detection on a string of bits of variable length (data-length considered) */
 #include<stdio.h>
 #include<stdlib.h>
-char divisor[6]="10011";//setting a divisor value
+char divisor[18]="1001100101000011";//setting a divisor value
 
 void crc(char a[])//function to calculate crc
 {
   int count=0,i=0,j;
   while(a[count]!='\0')count++;
-  while(i<count-5+1){
+  while(i<count-16){
     if(a[i]!='1') {
       i++;
       continue;
@@ -17,8 +17,10 @@ void crc(char a[])//function to calculate crc
       while(divisor[j]!='\0'){
         if(divisor[j]==a[i+j]) a[i+j]='0';
         else a[i+j]='1';
+        printf("%c",a[i+j]);
         j++;
       }
+      printf("\n");
       i++;
     }
   }
@@ -27,19 +29,19 @@ void crc(char a[])//function to calculate crc
 void main()
 {
   int i,j,ss=0,rs=0;
-  char sent[32],receive[32],cc[5];
+  char sent[128],receive[128],cc[17];
   printf("Enter the data being sent\n");
   gets(sent);
   while(sent[ss]!='\0') ss++;
-  for(i=ss;i<ss+4;i++) sent[i]='0';
+  for(i=ss;i<ss+16;i++) sent[i]='0';
   sent[i]='\0';
   printf("CRC is ");
   crc(sent);
-  i=ss+4;
-  j=4;
+  i=ss+16;
+  j=16;
   while(j>=0)
     cc[j--]=sent[i--];
-  cc[4]='\0';
+  cc[16]='\0';
   puts(cc);
   printf("Enter the data received\n");
   gets(receive);
@@ -54,8 +56,13 @@ void main()
   while(cc[j]!='\0')receive[i++]=cc[j++];
   receive[i]='\0';
   crc(receive);
-  printf("The remainder is %s, hence\n",receive);
-  for(i=0;i<rs+4;i++)
+  i=ss+16;
+  j=16;
+  while(j>=0)
+    cc[j--]=sent[i--];
+  cc[16]='\0';
+  printf("The remainder is %s, hence\n",cc);
+  for(i=0;i<rs+16;i++)
     if(receive[i]!='0'){
       printf("The data is corrupted\n");
       j=-1;
